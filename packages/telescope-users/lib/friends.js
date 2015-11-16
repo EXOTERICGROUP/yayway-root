58243daa82ea9b026202e7b76dd9bc0d41833d59
MFQ <softmfq@gmail.com>
2015-11-16 18:40:40 +0500

    code revamped

--- packages/telescope-users/lib/friends.js
+++ packages/telescope-users/lib/friends.js
@@ -1,2 +1,38 @@
 Users.requestsRef = new Mongo.Collection("requests");
 Users.friendsRef = new Mongo.Collection("friends");
+
+Users.friendsRef.schema = new SimpleSchema({
+  _id: {
+    type: String,
+    optional:true
+  },
+  createdAt: {
+    type: Date,
+    optional: true
+  },
+  requesterId: {
+    type:String
+  },
+  followingId: {
+    type: String
+  }
+});
+//
+var allowOnlyOwner = function(userId, doc){
+  if (userId && (doc.requesterId == userId) )
+    return true;
+  else
+    return false;
+}
+
+Users.friendsRef.allow({
+  insert: function(userId, doc){
+    return allowOnlyOwner(userId, doc)
+  },
+  update: function(userId, doc, fields, modifier){
+    return allowOnlyOwner(userId, doc)
+  },
+  remove: function(userId, doc){
+    return allowOnlyOwner(userId, doc)
+  }
+});
--- packages/telescope-users/lib/server/publications.js
+++ packages/telescope-users/lib/server/publications.js
@@ -93,21 +93,3 @@ Users.requestsRef.allow({
     return true;
   }
 });
-// friendsRef = new Mongo.Collection("friends");
-Users.friendsRef.allow({
-  insert: function (userId, doc) {
-    // the user must be logged in, and the document must be owned by the user
-    // return (userId && doc.owner === userId);
-    return true;
-  },
-  update: function (userId, doc, fields, modifier) {
-    // can only change your own documents
-    // return doc.owner === userId;
-    return true;
-  },
-  remove: function (userId, doc) {
-    // can only remove your own documents
-    // return doc.owner === userId;
-    return true;
-  }
-});
